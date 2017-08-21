91194a30d7a1f57035077ea79c0078c96adef8bc
mromaniszyn <maciej@ek-system.home.pl>
2017-08-21 21:16:22 +0200

    Some enhancments
    - added button to copy Build name
    - fixed copying Works included in Build
    - fixed going to only one visible sprint

--- baseExtension.js
+++ baseExtension.js
@@ -158,6 +158,26 @@ var SFAA = function(){
 		{ text:"Triaged"},
 		{ text:"Waiting"}];
 		
+	var createCopyButton = (parentNode, description, title, callback, options) => {
+		options = options || {'copy': true};
+		var btn = SFAA.addAfter(parentNode, 'BUTTON');
+		btn.innerHTML = description;
+		btn.title = title;
+		
+		if(options.copy){
+			SFAA.addClick(btn, () => {
+				var txt = callback();
+				SFAA.copyText(txt);
+			});
+		}
+		if(options.click){
+			SFAA.addClick(btn, callback);
+		}
+		if(options.buttonType){
+			btn.type = options.buttonType;
+		}
+	};
+		
 	return {
 		consts: consts,
 		gE : docs.gE,
@@ -170,10 +190,11 @@ var SFAA = function(){
 		
 		getUrl : winds.getUrl,
 		
-		
 		copyText : copyTextToClipboard,
 		getView : getView,
 		
-		workStatuses : workStatuses
+		workStatuses : workStatuses,
+		
+		createCopyButton: createCopyButton
 	};
 }();
